e2570015e879c01b8051e7598959ddf2a670f284
Senpat <pgz11902@gmail.com>
2020-10-26 17:24:29 -0400

    newsapi js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
 
 
+var categories = new Set(["entertainment","sports","technology"]);
+
 async function refresh(event){
-	//var url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=c38d6166558c48afb790593c8526d39b';
 	let sourcesurl = 'https://newsapi.org/v2/sources?apiKey=c38d6166558c48afb790593c8526d39b';
 	
-	var req = new Request(sourcesurl);
-	let res = await fetch(req);
-	let data = await res.json();
-	console.log(data);
-		// .then(function(response) {
-		// 	return response.json();
-		// })
-		// .then(function(response) {
-		// 	console.log(response)
-		// })	
+	var sourcesreq = new Request(sourcesurl);
+	let sourcesres = await fetch(sourcesreq);
+	let sourcesdata = await sourcesres.json();
+	console.log(sourcesdata);
+
+	var entertainmentbool = document.getElementById("entertainmentbox").checked;
+	var sportsbool = document.getElementById("sportsbox").checked;
+	var technologybool = document.getElementById("technologybox").checked;
+	var allbool = document.getElementById("allbox").checked;
+
+	var newshtml = "";
+	//add that data to the string
+	sourcesdata.sources.forEach(function(source){
+		if(categories.has(source.category)){
+			if(allbool || document.getElementById(source.category+"box").checked){
+				//show the news\
+				newshtml += "<p>" + source.name + "</p>"
+			}
+		}
+	});
+
+	document.getElementById("news section").innerHTML = newshtml;
 }
 
-console.log("javascript")
+function otheronclick(clickedid){
+	console.log(clickedid)
+	if(document.getElementById(clickedid).checked){
+		document.getElementById("allbox").checked = false;
+	}
+}
+
+function allonclick(event){
+	console.log(document.getElementById("allbox").checked);
+	if(document.getElementById("allbox").checked){
+		document.getElementById("entertainmentbox").checked = false;
+		document.getElementById("sportsbox").checked = false;
+		document.getElementById("technologybox").checked = false;
+	}
+}
+
+
